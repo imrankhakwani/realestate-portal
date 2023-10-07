1d63931c7a558ccb09f5d76b032011c1ce022570
imrankhakwani <30284761+imrankhakwani@users.noreply.github.com>
2023-10-07 22:20:25 +0500

    Mongodb configured and User model created

--- api/index.js
+++ api/index.js
@@ -1,9 +1,21 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 import cors from 'cors'
+
+dotenv.config()
 
 const app = express()
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
+
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log('Connected to mongo db server')
+  })
+  .catch((error) => {
+    console.error('Error connecting to mongo db server', error.message)
+  })
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000')
